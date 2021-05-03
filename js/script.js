80c285b97b8b13a1620d410959ba896efb7e9a8d
Shasi Kanth Dulipalla <shasi.dulipalla@valmont.com>
2021-05-03 09:14:03 +0530

    JS Course

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//alert("Hello world");
+
+// console is useful for developer to print some useful information to the browser console.
+console.log(3 + 3);
+document.write("Hello!");
+
+// Data Types
+// Numeric
+// Strings
+// boolean (True or False)
+
+var a = 100034;
+var A = 154234234;
+var b = "hello";
+var c = true;
+var d;
+d = 10.3;
+
+//console.log(a + A);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let e = 30;
+const f = 40;
+
+console.log(e);
+console.log(f);
